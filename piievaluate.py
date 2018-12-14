490087275a25b9b39789837c35d6231089ac1500
rama159065 <ramakris99@gmail.com>
2018-12-14 23:46:43 +0530

    commit model predict changes

--- piievaluate.py
+++ piievaluate.py
@@ -18,7 +18,7 @@ actual_df = dr.mergeData(testDir)
 print("actual df is ", actual_df)
 
 prob_df = pd.DataFrame.from_records(predictions_proba_eval, columns=['email_prob',  'guid_prob',  'ip_prob',  'phone_prob'])
-print("prob df is", prob_df)
+print("prob df is\n", prob_df)
 
 pred_df = pd.DataFrame(predictions_eval, columns=['prediction'])
 
@@ -26,7 +26,7 @@ input_data_columns=['email','gid','ip','phone']
 pred_df['label'] = pd.Categorical.from_codes(predictions_eval,input_data_columns)
 
 final_df = pd.concat([actual_df, prob_df, pred_df], axis=1)
-print("final df is ", final_df)
+print("final df is \n", final_df)
 
 #writing test data,prediction prob, prediction, prediction label to csv file
 final_df.to_csv('D:\\python_examples\\PII_DATA\\predict_proba\\pii_prob.csv',index=False, header=True, decimal='.', sep=',', float_format='%.2f')
